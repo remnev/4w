acaa3b87b92e8f278704ce39866c293ff619424c
Yuriy Remnev <yura_remnev@mail.ru>
2015-03-09 19:51:04 +0300

    Add block content

---
+++ bundles/index/blocks/content/content.bh.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function (bh) {
+
+    bh.match('content', function (ctx) {
+        ctx.content([
+            {
+                block: 'products',
+                mods: {type: 'prof'}
+            },
+            {
+                block: 'products',
+                mods: {type: 'home'}
+            }
+        ]);
+    });
+
+};
