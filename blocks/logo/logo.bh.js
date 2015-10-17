e0fbeeca6c8d3032a47ff418e8727869c3c823d8
Yuriy Remnev <yura_remnev@mail.ru>
2015-10-17 14:13:17 +0300

    Fixed logo horiz alignment

--- blocks/logo/logo.bh.js
+++ blocks/logo/logo.bh.js
@@ -1,18 +1,24 @@
 'use strict';
 
 module.exports = function (bh) {
-    var text = ['магазин', 'оконных принадлежностей'].join('<br/>');
+    var text = ['магазин', 'оконных', 'принадлежностей'].join('<br/>');
 
     bh.match('logo', function () {
         return {
             block: 'link',
             url: '/',
             mix: {block: 'logo'},
-            content: {
-                block: 'logo',
-                elem: 'text',
-                content: text
-            }
+            content: [
+                {
+                    block: 'logo',
+                    elem: 'image'
+                },
+                {
+                    block: 'logo',
+                    elem: 'text',
+                    content: text
+                }
+            ]
         };
     });
 };
