435f856a4d110034dd8557be99f45fd5c747cd67
Yuriy Remnev <yura_remnev@mail.ru>
2016-03-15 10:57:16 +0300

    Fixed some update script

--- updates/1.2.0-yandex-market.js
+++ updates/1.2.0-yandex-market.js
@@ -1,11 +1,13 @@
 exports.create = {
-    YandexMarketInfo: {
-        name: '4window',
-        company: 'Магазин оконных принадлежностей ООО "Проформ"',
-        url: 'http://4window.ru',
-        deliveryOptions: {
-            days: 2,
-            cost: 500
+    YandexMarketInfo: [
+        {
+            name: '4window',
+            company: 'Магазин оконных принадлежностей ООО "Проформ"',
+            url: 'http://4window.ru',
+            deliveryOptions: {
+                days: 2,
+                cost: 500
+            }
         }
-    }
+    ]
 };
